59f11c79679aacf6705207e454b8900e43065d17
hf <161416756+waterdude551@users.noreply.github.com>
2026-04-16 00:00:59 -0400

    revised slash animation + hitstop (#131)
    * Added trail to bullet
    
    Dash has an issue with the trail where the width resets after the animation.
    
    * Adjusted bullet and disabled depth of field
    
    DoF was messing with VFX...
    
    * layers
    
    * revise slash anim + hitstop
    
    * animation update
    
    * sword tweaks (less transition duration)
    
    * turned depth of field back on
    
    ---------
    
    Co-authored-by: Arshiya Rahman <127697399+arahman302@users.noreply.github.com>
    Co-authored-by: Grace-Shao <gshao8@gatech.edu>

--- Assets/Scripts/Player/Player Weapons/Player_Melee.cs
+++ Assets/Scripts/Player/Player Weapons/Player_Melee.cs
@@ -1,13 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player_Melee : Weapon
 {
     private PlayerStateMachine playerContext;
+
+    [SerializeField]
+    [Range(0f, 0.33f)]
+    float _hitStopDuration = 0.1f;
+    [SerializeField]
+    [Range(0f, 0.33f)]
+    float _delayDuration = 0.1f;
+
+    bool _isHitStopped = false;
+
     protected override void Init()
     {
         weilder = GameObject.FindGameObjectWithTag("Player").transform;
         playerContext = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStateMachine>();
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         string layer = LayerMask.LayerToName(other.gameObject.layer);
@@ -18,8 +30,34 @@ public class Player_Melee : Weapon
             {
                 playerContext.updateEnergy(playerContext.AttackGain);
                 Attack(damageable);
+
+                DoHitStop();
             }
         }
     }
 
+    void DoHitStop()
+    {
+        if (!_isHitStopped)
+        {
+            StartCoroutine(DelayCoroutine());
+        }
+    }
+
+    IEnumerator DelayCoroutine()
+    {
+        _isHitStopped = true;
+        yield return new WaitForSecondsRealtime(_delayDuration);
+        StartCoroutine(HitStopCoroutine());
+    }
+    IEnumerator HitStopCoroutine()
+    {
+        float timeScale = Time.timeScale;
+        Time.timeScale = 0f;
+
+        yield return new WaitForSecondsRealtime(_hitStopDuration);
+
+        Time.timeScale = timeScale;
+        _isHitStopped = false;
+    }
 }
