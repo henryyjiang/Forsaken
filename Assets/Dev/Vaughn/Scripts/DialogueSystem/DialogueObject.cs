6d9e3963b89dc40cbd197f063a403e92e7d4569b
VaughnCasarez <ccasarez3@gatech.edu>
2026-04-18 14:42:27 -0400

    Full Dialogue UI and Functionality Update (#137)
    * Dialogue and Interact System
    
    - Added Rough Dialogue UI
    - Added Modular Dialogue Integration
    - Added Modular Branching Dialogue
    - Added the Interact Button into Player Inputs
    
    * Updated Dialogue UI and Functionality
    
    * fix player getting stuck
    
    ---------
    
    Co-authored-by: Arshiya Rahman <127697399+arahman302@users.noreply.github.com>

--- Assets/Dev/Vaughn/Scripts/DialogueSystem/DialogueObject.cs
+++ Assets/Dev/Vaughn/Scripts/DialogueSystem/DialogueObject.cs
@@ -1,13 +1,23 @@
-using System.Net;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Dialogue/DialogueObject")]
 public class DialogueObject : ScriptableObject
 {
-    [SerializeField] [TextArea] private string[] dialogue;
+    [System.Serializable]
+    public class DialogueLine
+    {
+        public string name;
+        public Sprite portrait;
+        [TextArea] public string text;
+    }
+
+    [SerializeField] private Sprite characterPortrait;
+    [SerializeField] private DialogueLine[] dialogue;
     [SerializeField] private Response[] responses;
 
-    public string[] Dialogue => dialogue;
+    public Sprite CharacterPortrait => characterPortrait;
+
+    public DialogueLine[] Dialogue => dialogue;
 
     public bool HasResponses => Responses != null && Responses.Length > 0;
 
